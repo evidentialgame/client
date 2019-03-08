9b32c0b9a8a4e401747aaebd9a6e6ba0893f56bc
Ethan Davis <futuristiccake@gmail.com>
2019-03-07 22:31:28 -0800

    Add menu, debug mode

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
 const path = require('path')
 
-const {app, BrowserWindow} = require('electron')
+const args = require('gar')(process.argv.slice(2))
+
+const {app, BrowserWindow, Menu} = require('electron')
 
 app.on('ready', () => {
 	const win = new BrowserWindow({
+		'title': 'Evidential',
 		'webPreferences': {
 			'nodeIntegration': true
 		},
 		'width': 1920 * (0.8),
 		'height': 1080 * (0.8),
 		'resizable': false,
-		'title': 'Evidential'
+		'fullscreen': true
 	})
+
+	const menuTemplate = [
+		{
+			'label': 'File',
+			'submenu': [
+				{
+					'role': 'toggleFullScreen'
+				},
+				{
+					'role': 'quit'
+				}
+			]
+		}
+	]
+
+	if (args.debug === true) {
+		menuTemplate[0].submenu.push({
+			'role': 'toggleDevTools'
+		})
+	}
+
+	const barMenu = Menu.buildFromTemplate(menuTemplate)
+
+	win.setMenu(barMenu)
 
 	win.loadURL('file://' + path.join(__dirname, 'static', 'index.html'))
 	
